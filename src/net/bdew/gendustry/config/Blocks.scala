1abdfba03f28714456d5b51594b74f67ba780a2f
bdew <blackdew@gmail.com>
2014-01-13 20:35:44 +0200

    Removed test power provider

--- src/net/bdew/gendustry/config/Blocks.scala
+++ src/net/bdew/gendustry/config/Blocks.scala
@@ -9,17 +9,12 @@
 
 package net.bdew.gendustry.config
 
-import cpw.mods.fml.common.registry.GameRegistry
 import net.bdew.gendustry.mutagen.BlockMutagen
-import net.bdew.gendustry.test.PowerEmitterBlock
-import net.bdew.gendustry.test.PowerEmitterTile
 import net.bdew.lib.config.BlockManager
 import net.bdew.gendustry.Gendustry
 
 object Blocks extends BlockManager(Config.IDs) {
   val mutagen = regBlockCls(classOf[BlockMutagen], "Mutagen")
-  val testPowerEmitter = new PowerEmitterBlock(ids.getBlockId("Test Power Emitter"))
-  GameRegistry.registerBlock(testPowerEmitter, "PowerEmitterBlock")
-  GameRegistry.registerTileEntity(classOf[PowerEmitterTile], "PowerEmitterBlock")
+
   Gendustry.logInfo("Blocks loaded")
 }
