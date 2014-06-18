231be61c98fa314f569f326209aaa47625c4e992
bdew <blackdew@gmail.com>
2014-06-18 11:52:26 +0300

    Reinstated RF support (#25)
    This reverts commit ada04a64e1805d004587ee12fac4a81728b3d9a1.

---
+++ src/net/bdew/gendustry/power/TilePoweredRF.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) bdew, 2013 - 2014
+ * https://github.com/bdew/gendustry
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * https://raw.github.com/bdew/gendustry/master/MMPL-1.0.txt
+ */
+
+package net.bdew.gendustry.power
+
+import cofh.api.energy.IEnergyHandler
+import net.bdew.gendustry.config.Tuning
+import cpw.mods.fml.common.Optional
+import net.bdew.gendustry.compat.PowerProxy
+import net.bdew.lib.power.TilePoweredBase
+import net.minecraftforge.common.util.ForgeDirection
+
+@Optional.Interface(modid = PowerProxy.TE_MOD_ID, iface = "cofh.api.energy.IEnergyHandler")
+trait TilePoweredRF extends TilePoweredBase with IEnergyHandler {
+  private lazy val ratio = Tuning.getSection("Power").getFloat("RF_MJ_Ratio")
+
+  override def receiveEnergy(from: ForgeDirection, maxReceive: Int, simulate: Boolean) =
+    (power.inject(maxReceive / ratio, simulate) * ratio).floor.toInt
+
+  override def extractEnergy(from: ForgeDirection, maxExtract: Int, simulate: Boolean) = 0
+  override def canConnectEnergy(from: ForgeDirection) = PowerProxy.RFEnabled
+  override def getEnergyStored(from: ForgeDirection) = (power.stored * ratio).floor.toInt
+  override def getMaxEnergyStored(from: ForgeDirection) = (power.capacity * ratio).floor.toInt
+}
