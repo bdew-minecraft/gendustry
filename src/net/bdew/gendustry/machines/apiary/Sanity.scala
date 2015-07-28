1330cdf63af176de1366a957057e7af0dd8f4885
bdew <blackdew@gmail.com>
2015-07-28 13:08:38 +0300

    Fixed apiary sanity check

--- src/net/bdew/gendustry/machines/apiary/Sanity.scala
+++ src/net/bdew/gendustry/machines/apiary/Sanity.scala
@@ -9,7 +9,7 @@
 
 package net.bdew.gendustry.machines.apiary
 
-import _root_.forestry.api.apiculture.IBeeRoot
+import forestry.api.apiculture.IBeeRoot
 import forestry.api.genetics.AlleleManager
 import net.bdew.gendustry.Gendustry
 import net.minecraft.util.{ChatComponentTranslation, EnumChatFormatting}
@@ -26,7 +26,7 @@ object Sanity {
       val defSpecies = defGenome.getPrimary
       var found = false
       for (mutation <- bees.getMutations(false)) {
-        if (mutation.getChance(house, defSpecies.asInstanceOf, defSpecies, defGenome, defGenome) > 0) {
+        if (mutation.getChance(house, defSpecies, defSpecies, defGenome, defGenome) > 0) {
           found = true
           Gendustry.logWarn("Detected probably bugged mutation! %s+%s (class: %s) doesn't check the species. Please report it to the mod author.",
             mutation.getAllele0.getName, mutation.getAllele1.getName, mutation.getClass.getCanonicalName)
