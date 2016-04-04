4aebc024ae9bd390ab70375b72e1f3521227c4c5
bdew <blackdew@gmail.com>
2016-04-04 13:03:10 +0300

    Removed cofh and mekanism itempush for now

--- src/net/bdew/gendustry/compat/itempush/ItemPush.scala
+++ src/net/bdew/gendustry/compat/itempush/ItemPush.scala
@@ -12,7 +12,7 @@ package net.bdew.gendustry.compat.itempush
 import net.bdew.lib.Misc
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
-import net.minecraftforge.common.util.ForgeDirection
+import net.minecraft.util.EnumFacing
 
 object ItemPush {
   var proxies = List.empty[ItemPushProxy]
@@ -20,13 +20,13 @@ object ItemPush {
   def init() {
     register(VanillaPush)
     if (Misc.haveModVersion("BuildCraftAPI|transport@[3.0,)")) register(BCPipePushProxy)
-    if (Misc.haveModVersion("CoFHAPI|transport")) register(CofhConduitPushProxy)
-    if (Misc.haveModVersion("Mekanism")) register(MekanismPushProxy)
+    //    if (Misc.haveModVersion("CoFHAPI|transport")) register(CofhConduitPushProxy)
+    //    if (Misc.haveModVersion("Mekanism")) register(MekanismPushProxy)
   }
 
   def register(p: ItemPushProxy) = proxies :+= p
 
-  def pushStack(from: TileEntity, dir: ForgeDirection, st: ItemStack) = {
+  def pushStack(from: TileEntity, dir: EnumFacing, st: ItemStack) = {
     var stack = st
     for (proxy <- proxies if stack != null && stack.stackSize > 0)
       stack = proxy.pushStack(from, dir, stack)
